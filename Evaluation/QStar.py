223f712fc76ca5123693769952b0eb1de7863036
RearrangedLetters <RobinLink@ProtonMail.com>
2022-07-01 21:34:29 +0200

    Represent field extension of interest and its properties in the project.

---
+++ Evaluation/QStar.py
@@ -0,0 +1,12 @@
+###
+# Q_* is the adjunction of {omega, theta} to Q, where omega is a third root of unity
+# and omega is the cube root of 2. The result is a six-dimensional vector space over
+# Q. This file collects same basic properties of this extension.
+###
+import numpy as np
+
+from src import RootOfUnity
+
+omega = RootOfUnity.root_of_unity(3)
+theta = np.cbrt(2)
+B = (1, theta, theta ** 2, omega, omega * theta, omega * (theta ** 2))
