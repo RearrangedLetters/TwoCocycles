eb6e94e795ad6a6300f583b4d5e404e7ae14286e
RearrangedLetters <RobinLink@ProtonMail.com>
2022-07-02 17:19:06 +0200

    Implement Cayley table of Galois group G.

--- Evaluation/G/GroupG.py
+++ Evaluation/G/GroupG.py
@@ -1,10 +1,8 @@
 ###
 # The Galois group G of Q_* over Q.
 ###
-from Evaluation.QStar import *
-from src.FieldAutomorphism import FieldAutomorphism
-from Evaluation.C import GroupC as C
-from Evaluation.D import GroupD as D
+from Evaluation.C.GroupC import *
+from Evaluation.D.GroupD import *
 
 c = C.c
 d = D.d
@@ -14,9 +12,9 @@ cd2 = FieldAutomorphism(B, lambda x: c.apply(d2.apply(x)))
 
 G = (id, d, d2, c, cd, cd2)
 
-cayley_table = [[0, 0, 0, 0, 0, 0],  # todo: not correct yet
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0]]
+cayley_table = [[0, 1, 2, 3, 4, 5],
+                [1, 2, 0, 5, 3, 4],
+                [2, 0, 1, 4, 5, 3],
+                [3, 4, 5, 0, 1, 2],
+                [4, 5, 3, 2, 0, 1],
+                [5, 3, 4, 1, 2, 0]]
