d1bc6b4dacd17dea6a944ed7ce8eac2c7b9dc038
RearrangedLetters <RobinLink@ProtonMail.com>
2022-07-26 01:11:05 +0200

    Improve latex table formatting.

--- Evaluation/Auxiliary/LatexTools.py
+++ Evaluation/Auxiliary/LatexTools.py
@@ -3,19 +3,19 @@ import numpy as np
 from Parser import parse_roots
 
 
-def to_latex_tabular(line=624):  # This corresponds to the root starting on the actual line 573
+def to_latex_tabular(line=1):  # This corresponds to the root starting on the actual line 573
     index = int(line / 17)
     roots = parse_roots(26, "../G/twococycles")
     root = roots[index].root
-    number_format = "{:10.7f}"
+    number_format = "{:10.4f}"
     q = complex(root[25][0], root[25][1])
-    q_str = number_format.format(q)
-    q_conj_str = number_format.format(complex(q.real, -q.imag))
+    q_str = " $ " + number_format.format(q).replace("j", "i") + " $ "
+    q_conj_str = "$ " + number_format.format(complex(q.real, -q.imag)) + " $"
     R = np.array([number_format.format(complex(r[0], r[1])) for r in root[:25]])
     R = R.reshape(5, 5)
     table_header = "\\begin{tabular}{rcccccc}" \
                    "\n\t& $ 1 $ & $ d $ & $ d^2 $ & $ c $ & $ cd $ & $ cd^2 $ \\\\\\hline\n" \
-                   + "\t$ 1 $ & " + ((q_str + " & ") * 6)[:-2] + " \\\\\n"
+                   + "\t$ 1 $ &" + ((q_str + "&") * 6)[:-2] + " \\\\\n"
     table_body = ""
     group_element_names = ["$ 1 $", "$ d $", "$ d^2 $", "$ c $", "$ cd $", "$ cd^2 $"]
     for i, row in enumerate(R):
