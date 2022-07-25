a524b76c5af73bbc839fe8c84bdc720a679831ff
RearrangedLetters <RobinLink@ProtonMail.com>
2022-07-25 22:06:19 +0200

    Add parser functionality to get a 2-cocycle from a numerically found and textually saved solution.

--- Evaluation/Auxiliary/Parser.py
+++ Evaluation/Auxiliary/Parser.py
@@ -1,6 +1,12 @@
 import re
 
+import numpy as np
+
 from Evaluation.Auxiliary.Solution import Solution
+from Evaluation.C.GroupC import conjugate, C
+from Evaluation.QStar import B
+from Evaluation.Tools import extend_each_coordinate, extend_coordinates
+from src.TwoCocycle import TwoCocycle
 
 
 def parse_roots(n, file="twococycles"):
@@ -37,3 +43,19 @@ def count_empty(file="twococycles"):
             if not line.strip():
                 count += 1
     return count
+
+
+def twococycle_from_root(solution):
+    root = solution.root
+    q = extend_coordinates(root[25])
+    R = np.array(root[:25])
+    R = R.reshape((5, 5, 2))
+    R = extend_each_coordinate(R)
+    conjugate = C[1].apply
+    mapping = [[q, q, q, q, q, q],
+               [conjugate(q), R[0][0], R[0][1], R[0][2], R[0][3], R[0][4]],
+               [conjugate(q), R[1][0], R[1][1], R[1][2], R[1][3], R[1][4]],
+               [conjugate(q), R[2][0], R[2][1], R[2][2], R[2][3], R[2][4]],
+               [conjugate(q), R[3][0], R[3][1], R[3][2], R[3][3], R[3][4]],
+               [conjugate(q), R[4][0], R[4][1], R[4][2], R[4][3], R[4][4]]]
+    return TwoCocycle(B, mapping)
