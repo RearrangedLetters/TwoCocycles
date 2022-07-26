0b5e9770b8bd582523eeea5e6983dabc8b617e99
RearrangedLetters <RobinLink@ProtonMail.com>
2022-07-26 14:40:18 +0200

    Fix bugs in table generation.

--- Evaluation/Auxiliary/LatexTools.py
+++ Evaluation/Auxiliary/LatexTools.py
@@ -1,29 +1,28 @@
 import numpy as np
 
-from Parser import parse_roots
+from Parser import parse_roots, twococycle_from_root
+
+number_format = "{:1.5f}"
 
 
-def to_latex_tabular(line=1):  # This corresponds to the root starting on the actual line 573
+def matrix_to_latex_tablebody(row_labels, M):
+    body = ""
+    for i, r in enumerate(M):
+        row = ""
+        for j in r:
+            entry = complex(j[0], j[3])
+            row = row + " & $" + number_format.format(entry).replace("j", "i") + "$"
+        body = body + "\t" + row_labels[i] + row + "\\\\\n"
+    return body.replace("  ", " ")
+
+
+def to_latex_tabular(line=33):
     index = int(line / 17)
-    roots = parse_roots(26, "../G/twococycles")
-    root = roots[index].root
-    number_format = "{:10.4f}"
-    q = complex(root[25][0], root[25][1])
-    q_str = " $ " + number_format.format(q).replace("j", "i") + " $ "
-    q_conj_str = "$ " + number_format.format(complex(q.real, -q.imag)) + " $"
-    R = np.array([number_format.format(complex(r[0], r[1])) for r in root[:25]])
-    R = R.reshape(5, 5)
+    twococycle = twococycle_from_root(parse_roots(26, "../G/twococycles")[index])
     table_header = "\\begin{tabular}{rcccccc}" \
-                   "\n\t& $ 1 $ & $ d $ & $ d^2 $ & $ c $ & $ cd $ & $ cd^2 $ \\\\\\hline\n" \
-                   + "\t$ 1 $ &" + ((q_str + "&") * 6)[:-2] + " \\\\\n"
-    table_body = ""
-    group_element_names = ["$ 1 $", "$ d $", "$ d^2 $", "$ c $", "$ cd $", "$ cd^2 $"]
-    for i, row in enumerate(R):
-        table_row = q_conj_str
-        for j in row:
-            table_row = table_row + " & " + "$ " + j + " $"
-        table_body = table_body + "\t" + group_element_names[i + 1] + " & " + table_row + " \\\\ \n"
-    table_body = table_body.replace("j", "i")
+                   "\n\t& $1$ & $d$ & $d^2$ & $c$ & $cd$ & $cd^2$ \\\\\\hline\n"
+    group_element_names = ["$1$", "$d$", "$d^2$", "$c$", "$cd$", "$cd^2$"]
+    table_body = matrix_to_latex_tablebody(group_element_names, twococycle.mapping)
     table_footer = "\\end{tabular}"
     return table_header + table_body + table_footer
 
